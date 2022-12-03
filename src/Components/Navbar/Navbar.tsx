d895a8f92f20dd1ba2d5a92460c1e4be329e6930
Kwon Valerii <wellko24@gmail.com>
2022-12-03 16:06:39 +0600

    added Navbar.tsx Navigation

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <NavLink to='/' className="navbar-brand">Quotes</NavLink>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <NavLink to='/add' className="nav-link">Add</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to='/' className="nav-link">Show quotes</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
