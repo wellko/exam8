4c187de1cd21ef17181a0ddd2be6c9fba2546905
Kwon Valerii <wellko24@gmail.com>
2022-12-03 16:04:33 +0600

    Created container App with all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Navbar from "./Components/Navbar/Navbar";
+import {Route, Routes} from "react-router-dom";
+import QuotesBlock from "./Components/QuotesBlock/QuotesBlock";
+import Form from "./Components/Form/Form";
+import HomePage from "./Containers/HomePage/HomePage";
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<HomePage/>}>
+                    <Route path='/quotes/:category' element={<QuotesBlock/>}/>
+                </Route>
+                <Route path='/quotes/:id/edit' element={<Form/>}/>
+                <Route path='/add' element={<Form/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
