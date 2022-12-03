6e673718cf43a98d3137e297d7a8becf7a1128b6
Kwon Valerii <wellko24@gmail.com>
2022-12-03 16:06:14 +0600

    created Main page container

---
+++ src/Containers/HomePage/HomePage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import SideBar from "../../Components/SideBar/SideBar";
+import QuotesBlock from "../../Components/QuotesBlock/QuotesBlock";
+
+
+const HomePage = () => {
+    return (
+        <div className="container-fluid">
+            <div className="row flex-nowrap">
+                <SideBar/>
+                <main className="col ps-md-2 pt-2">
+                    <QuotesBlock/>
+                </main>
+            </div>
+        </div>
+    );
+};
+
+
+export default HomePage;
